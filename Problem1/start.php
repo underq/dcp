b36298e67a0d52e1fe36fed4ed38b83356ed158b
Benjamin <benjamin.d42@gmail.com>
2022-12-12 12:13:55 +0100

    Problem 1: Fix a test case

--- Problem1/start.php
+++ Problem1/start.php
@@ -7,7 +7,8 @@ function execute(array $array, int $k): bool {
     for ($i = 0; $i < count($array); $i++) {
         $searchValue = $k - $array[$i];
 
-        for ($j = $i; $j < count($array) - $i; $j++) {
+        for ($j = $i + 1; $j < count($array); $j++) {
+            //echo $j . " : " . $array[$j] . PHP_EOL;
             if ($searchValue === $array[$j]) {
                 return true;
             }
@@ -23,6 +24,7 @@ assertT(execute([10, 15, 3, 7], 17), true);
 assertT(execute([10, 15, 3, 7, 9, 8, 12, 31, 45], 55), true);
 
 assertT(execute([], 56), false);
+assertT(execute([1, 2, 3, 4], 2), false);
 assertT(execute([1, 2, 3, 4], 8), false);
 assertT(execute([1, 2, 3, 4], 9), false);
 assertT(execute([10, 15, 3, 7, 9, 8, 12, 31, 45], 56), false);
